393901f12e571b7c6620d1f2bb293ac5dfb4eac0
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2021-08-22 16:39:43 -0500

    Finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,17 +7,82 @@ function App() {
     email: ""
   });
 
+  const [submittedContact, setSubmittedContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function onChange(event) {
+    const { name, value } = event.target;
+    console.log(name);
+    console.log(value);
+    switch (name) {
+      case "fName":
+        setContact({
+          fName: value,
+          lName: contact.lName,
+          email: contact.email
+        });
+        break;
+
+      case "lName":
+        setContact({
+          fName: contact.fName,
+          lName: value,
+          email: contact.email
+        });
+        break;
+
+      case "email":
+        setContact({
+          fName: contact.fName,
+          lName: contact.lName,
+          email: value
+        });
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  function onSubmit(event) {
+    event.preventDefault();
+    setSubmittedContact(contact);
+    setContact({
+      fName: "",
+      lName: "",
+      email: ""
+    });
+  }
+
   return (
     <div className="container">
       <h1>
-        Hello {contact.fName} {contact.lName}
+        Hello {submittedContact.fName} {submittedContact.lName}
       </h1>
-      <p>{contact.email}</p>
+      <p>{submittedContact.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
-        <button>Submit</button>
+        <input
+          name="fName"
+          placeholder="First Name"
+          onChange={onChange}
+          value={contact.fName}
+        />
+        <input
+          name="lName"
+          placeholder="Last Name"
+          onChange={onChange}
+          value={contact.lName}
+        />
+        <input
+          name="email"
+          placeholder="Email"
+          onChange={onChange}
+          value={contact.email}
+        />
+        <button onClick={onSubmit}>Submit</button>
       </form>
     </div>
   );
